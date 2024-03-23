99faf42050df6f72201437eb748a35c9fff1f55e
tdedetd <tdedetd@gmail.com>
2024-03-24 00:27:30 +0300

    carousel

--- src/typescript/index.ts
+++ src/typescript/index.ts
@@ -1,20 +1,51 @@
+import { Carousel } from './libs/carousel/carousel.class';
 import { ViewportEvents } from './libs/viewport-events/viewport-events.class';
 
-document.addEventListener('DOMContentLoaded', () => {
-  new ViewportEvents([
-    {
-      action: 'add-class',
-      classList: ['animated'],
-      selector: '.transparent',
-      threshold: 1,
-      delay: 100
-    },
-    {
-      action: 'add-class',
-      classList: ['animated'],
-      selector: '.motion-up',
-      threshold: 1,
-      delay: 100
-    },
-  ]);
-});
+initCarousels();
+initViewportEvents();
+
+function initCarousels(): void {
+  window.addEventListener('load', () => {
+    new Carousel<HTMLImageElement>({
+      dots: {
+        containerId: 'carousel-projects-dots',
+        itemClass: 'carousel-projects-dot',
+        onItemChange: ({ dotFrom, dotTo }): void => {
+          const dotSelectedClass = 'carousel-projects-dot_selected';
+          dotFrom?.classList.remove(dotSelectedClass);
+          dotTo.classList.add(dotSelectedClass);
+        }
+      },
+      items: {
+        containerId: 'carousel-projects-images',
+        onSelectItem: ({ container, item, relativeIndex }): void => {
+          const sizeCoef = relativeIndex === 0 ? 1 : 0.75;
+          const x = container.clientWidth / 2 - item.clientWidth / 2 + relativeIndex * 700;
+          item.style.transform = `translateX(${x}px) scale(${sizeCoef})`;
+        },
+        selectedIndex: 1,
+      }
+    });
+  });
+}
+
+function initViewportEvents(): void {
+  document.addEventListener('DOMContentLoaded', () => {
+    new ViewportEvents([
+      {
+        action: 'add-class',
+        classList: ['animated'],
+        selector: '.transparent',
+        threshold: 1,
+        delay: 100
+      },
+      {
+        action: 'add-class',
+        classList: ['animated'],
+        selector: '.motion-up',
+        threshold: 1,
+        delay: 100
+      },
+    ]);
+  });
+}
